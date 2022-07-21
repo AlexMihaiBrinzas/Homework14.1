0ae488461524731a22f8508ee788cf0f22d3bbc1
Alex <brinzasalex@gmail.com>
2022-07-21 18:30:12 +0300

    Initial commit

---
+++ src/com/fasttrack/Quote.java
@@ -0,0 +1,36 @@
+package com.fasttrack;
+
+public class Quote {
+
+    private int id;
+
+    private String author;
+
+    private String quote;
+
+    private boolean favourite;
+
+    public Quote(int id, String author, String quote, boolean favourite) {
+        this.id = id;
+        this.author = author;
+        this.quote = quote;
+        this.favourite = favourite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+}
